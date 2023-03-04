52ac5a604e91d64aed323dffa1631a05c4ea4eae
unknown <visconti.joao@gmail.com>
2023-03-04 11:58:14 +0000

    Initialize Firebase app, fix web and android configurations

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:training_app/responsive/mobile_screen_layout.dart';
 import 'package:training_app/responsive/responsive_layout_screen.dart';
 import 'package:training_app/responsive/web_screen_layout.dart';
 import 'package:training_app/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: 'AIzaSyCTkfZ9YEGebqtQLYkfyYAywmlxWxJgIek',
+        appId: "1:645974518800:web:e871888435e92e3a96ae95",
+        messagingSenderId: "645974518800",
+        projectId: "training-app-13487",
+        storageBucket: "training-app-13487.appspot.com",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
